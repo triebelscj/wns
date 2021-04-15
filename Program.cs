303b498fcbfda804123723010e83093992acaef3
triebelscj <cjtriebels208@gmail.com>
2021-04-14 17:27:37 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using HumanProject.Models;
+
+namespace HumanProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human Zoe = new Human("Zoe");
+            Human Brent = new Human("Brent");
+            Human Shirley = new Human("Shirley");
+
+            Wizard Cj = new Wizard("Cj");
+            Ninja Sam = new Ninja("Sam");
+            Samurai Lisa = new Samurai("Lisa");
+
+            Cj.Attack(Zoe);
+            Sam.Attack(Shirley);
+            for (int i = 0; i < 6; i++)
+            {
+                Lisa.Attack(Brent);
+            }
+
+            Cj.FirstAid(Zoe);
+            Sam.Attack(Shirley);
+
+
+        }
+
+    }
+}
